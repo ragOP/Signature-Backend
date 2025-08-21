8f354464ab690ac4eb02767b6e7e2f6647e4ab58
farish-jamal <farishjamal98@gmail.com>
2025-08-21 11:37:37 +0530

    Add ABD order model and CRUD endpoints for order management

--- routers/lander4/index.js
+++ routers/lander4/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const orderModel4 = require("../../models/oderModel4");
 const crypto = require("crypto");
+const orderModel4Abd = require("../../models/orderModel4-abd");
 
 router.post("/create-order", async (req, res) => {
   const {
@@ -58,6 +59,68 @@ router.get("/get-orders", async (req, res) => {
     success: true,
     data: orders,
   });
+});
+
+/**
+ * POST /api/lander4/create-order-abd
+ */
+router.post("/create-order-abd", async (req, res) => {
+  const {
+    amount,
+    fullName,
+    email,
+    phoneNumber,
+    profession,
+    remarks,
+    additionalProducts = [],
+  } = req.body;
+
+  try {
+    const payload = {
+      abdOrderId: `ord_${Date.now()}_${Math.floor(Math.random() * 1e5)}`,
+      amount,
+      fullName: fullName,
+      email: email,
+      phoneNumber: phoneNumber,
+      profession: profession,
+      remarks: remarks,
+      additionalProducts: additionalProducts,
+    };
+    const order = await orderModel4Abd.create(payload);
+
+    return res.status(200).json({ success: true, data: order });
+  } catch (error) {
+    console.error("create-order-abd error:", error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/**
+ * DELETE /api/lander4/delete-order-abd/:id
+ */
+router.delete("/delete-order-abd/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!id) return res.status(400).json({ success: false, error: "id required" });
+
+  try {
+    const order = await orderModel4Abd.findByIdAndDelete(id);
+    return res.status(200).json({
+      success: true,
+      data: order,
+      message: "Abandoned Order deleted successfully",
+    });
+  } catch (error) {
+    console.error("delete-order-abd error:", error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/**
+ * GET /api/lander4/get-orders-abd
+ */
+router.get("/get-orders-abd", async (req, res) => {
+  const orders = await orderModel4Abd.find({}).sort({ createdAt: -1 });
+  return res.status(200).json({ success: true, data: orders });
 });
 
 module.exports = router;
