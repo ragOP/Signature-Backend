11b9e688f96cec57b77486ad6a24fc20ecc010a4
Nitin Chaudhary <nc1575612@gmail.com>
2025-09-28 21:25:05 +0530

    swagger url added

--- config/swagger.js
+++ config/swagger.js
@@ -10,11 +10,17 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:9005/api/project-0",
+        // url: "http://localhost:9005/api/project-0",
+        url: "https://signature-backend-bm3q.onrender.com/api/project-0",
       },
     ],
   },
-  apis: ["./routers/project_0/user/*.js", "./routers/project_0/company/*.js", "./routers/project_0/project/*.js", "./routers/project_0/task/*.js"],
+  apis: [
+    "./routers/project_0/user/*.js",
+    "./routers/project_0/company/*.js",
+    "./routers/project_0/project/*.js",
+    "./routers/project_0/task/*.js",
+  ],
 };
 
 const specs = swaggerJsdoc(options);
