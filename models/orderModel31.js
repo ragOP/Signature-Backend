96d9f1d631dd72e4153bb428097d9a9cc9f7a9e1
farish-jamal <farishjamal98@gmail.com>
2026-01-28 12:47:43 +0530

    feat: add lander31 order management with dedicated models and API routes.

---
+++ models/orderModel31.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    orderId: {
+        type: String,
+        required: true,
+    },
+    fullName: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    phoneNumber: {
+        type: String,
+    },
+    profession: {
+        type: String,
+    },
+    remarks: {
+        type: String,
+        default: "",
+    },
+    orderDate: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    razorpayOrderId: {
+        type: String,
+    },
+    razorpayPaymentId: {
+        type: String,
+    },
+    additionalProducts: {
+        type: [String],
+        default: [],
+    },
+    deliveryStatusEmail: {
+        type: Boolean,
+        default: false,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Order31", orderSchema);
