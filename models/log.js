109b45bdb52a54b667fc00b13352efb302c00b4f
farish-jamal <farishjamal98@gmail.com>
2026-02-19 19:21:44 +0530

    feat: update log model schema and rename to Log2

--- models/log.js
+++ models/log.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const logSchema = new mongoose.Schema({
+const logSchema2 = new mongoose.Schema({
     path: { type: String, required: true },
     timestamp: { type: String, required: true }
 });
 
-const Log = mongoose.model('Log', logSchema);
+const Log2 = mongoose.model('Log2', logSchema2);
 
-module.exports = Log;
+module.exports = Log2;
